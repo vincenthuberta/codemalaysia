485e03837174529aad979a4cbc066064cd3ab33b
Vincent Huberta <vincent.huberta@gmail.com>
2015-05-22 07:01:19 +0000

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   
+  
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
@@ -8,4 +9,19 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+  
+  class String
+  
+    def shuffle
+      self.split('').shuffle.join
+    end
+  end
+  
+  params = {father: "person1", mother: "person2", child: "person3"}
+  
+  params.each do |x,y| 
+    puts "Key #{x} has value #{y}"
+  end
+  
+  
 end
